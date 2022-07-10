f6b333df4da72cb0e29cb394471216f292e913d3
Victor Olave <victorolave1131@gmail.com>
2022-07-10 18:06:08 -0500

    Crud Brands

--- src/pages/Brands/components/Create.js
+++ src/pages/Brands/components/Create.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import {axiosApi} from "../../../helpers/api";
 
 const Create = (props) => {
+
+    const [name, setName] = useState('');
+    const [code, setCode] = useState('');
+
+    /**
+     * @desc Method to save a new brand.
+     * @returns {Promise<void>}
+     */
+    const save = async () => {
+        let data = {
+            name: name,
+            code: code
+        };
+
+        await axiosApi.post('brand', data)
+            .then(response => {
+                props.toggle();
+                props.getBrands();
+            })
+            .catch(error => console.log(error));
+    }
+
     return (
         <Modal isOpen={props.modal} toggle={props.toggle} centered>
             <ModalHeader toggle={props.toggle}>
@@ -10,11 +33,11 @@ const Create = (props) => {
             <ModalBody>
                 <FormGroup>
                     <Label for="name">Name</Label>
-                    <Input id="name" name="name" placeholder="Enter brand name..." type="text"/>
+                    <Input id="name" name="name" placeholder="Enter brand name..." type="text" value={name} onChange={(e) => setName(e.target.value)}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="code">Code</Label>
-                    <Input id="code" name="code" placeholder="Enter brand code..." type="text"/>
+                    <Input id="code" name="code" placeholder="Enter brand code..." type="text" value={code} onChange={(e) => setCode(e.target.value)}/>
                 </FormGroup>
             </ModalBody>
             <ModalFooter>
@@ -23,7 +46,7 @@ const Create = (props) => {
                 </Button> {' '}
                 <Button
                     color="primary"
-                    onClick={function noRefCheck(){}}
+                    onClick={save}
                 >
                     Save
                 </Button>
--- src/pages/Brands/components/Edit.js
+++ src/pages/Brands/components/Edit.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import {Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import {axiosApi} from "../../../helpers/api";
 
 const Edit = (props) => {
+
+    const { brand } = props;
+
+    const [name, setName] = useState('');
+    const [code, setCode] = useState('');
+
+    /**
+     * @desc Method to save the update.
+     * @returns {Promise<void>}
+     */
+    const save = async () => {
+        let data = {
+            name: name,
+            code: code
+        };
+
+        await axiosApi.put(`brand/${brand.id}`, data)
+            .then(response => {
+                props.toggle();
+                props.getBrands();
+            })
+            .catch(error => console.log(error));
+    }
+
+    const setData = () => {
+        setName(brand.name);
+        setCode(brand.code);
+    }
+
     return (
-        <Modal isOpen={props.modal} toggle={props.toggle} centered>
+        <Modal isOpen={props.modal} toggle={props.toggle} centered onOpened={setData}>
             <ModalHeader toggle={props.toggle}>
                 Edit Brand
             </ModalHeader>
             <ModalBody>
                 <FormGroup>
                     <Label for="name">Name</Label>
-                    <Input id="name" name="name" placeholder="Enter brand name..." type="text"/>
+                    <Input id="name" name="name" placeholder="Enter brand name..." type="text" value={name} onChange={(e) => setName(e.target.value)}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="code">Code</Label>
-                    <Input id="code" name="code" placeholder="Enter brand code..." type="text"/>
+                    <Input id="code" name="code" placeholder="Enter brand code..." type="text" value={code} onChange={(e) => setCode(e.target.value)}/>
                 </FormGroup>
             </ModalBody>
             <ModalFooter>
@@ -23,9 +53,9 @@ const Edit = (props) => {
                 </Button> {' '}
                 <Button
                     color="primary"
-                    onClick={function noRefCheck(){}}
+                    onClick={save}
                 >
-                    Save
+                    Update
                 </Button>
             </ModalFooter>
         </Modal>
